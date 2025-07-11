8ece1ca28b44f69a7c78ce70d5b106818af106ce
ThanoojKhan <thanu1495@gmail.com>
2025-07-11 16:58:16 +0530

    -Doctor card UI update -Cliic details UI updates -Reports UI fix

--- src/components/Atoms/Overview/Reports.jsx
+++ src/components/Atoms/Overview/Reports.jsx
@@ -7,7 +7,7 @@ const Reports = ({ weeklyAppointments, doctorRatios, todaySlots }) => {
 
     return (
         <div className="bg-white border p-6 rounded-lg min-h-[460px] border-solid my-5 shadow-lg border-[rgba(0,0,0,0.1)]">
-            <h2 className="text-[19px] font-bold mb-5  text-center">Appointmnet Reports</h2>
+            <h2 className="text-[19px] font-bold mb-5  text-center">Appointment Reports</h2>
 
             <div className="p-6 rounded-[14px] grid grid-cols-1 md:grid-cols-2 gap-6 items-center">
                 {/* <ExpensesDonutChart /> */}
