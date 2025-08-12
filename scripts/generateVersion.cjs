d0e7de151bed6f774a524d363019a8da0a37aab2
ThanoojKhan <thanu1495@gmail.com>
2025-08-12 17:42:15 +0530

    Auto update version check (trial 2)

--- scripts/generateVersion.cjs
+++
@@ -1,5 +0,0 @@
-const fs = require('fs');
-const { version } = require('../package.json');
-
-fs.writeFileSync('./public/version.json', JSON.stringify({ version }), 'utf8');
-console.log(`Version file created: ${version}`);
