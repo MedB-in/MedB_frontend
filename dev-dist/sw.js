e94beb9151719965c71b7549568c3a4bbd3fd8a7
ThanoojKhan <thanu1495@gmail.com>
2025-08-22 21:57:24 +0530

    auto update test 1

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.6hq79df1ld8"
+    "revision": "0.gli5t97950g"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
